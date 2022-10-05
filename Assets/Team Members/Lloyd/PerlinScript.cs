2ad81096985c01bcc19fe503f89b0f997b876ed1
trash <102205@student.sae.edu.au>
2022-10-05 15:49:20 +1100

    fixed script :O

--- Assets/Team Members/Lloyd/PerlinScript.cs
+++ Assets/Team Members/Lloyd/PerlinScript.cs
@@ -4,38 +4,30 @@ using UnityEngine;
 
 public class PerlinScript : MonoBehaviour
 {
-    
+
     public GameObject cubePrefab;
 
     private float x;
     private float y;
-    
+
     public Vector3 position;
-    
-    
-    
+
+
+
     private void Start()
     {
-            for (int j = 0; j < 100; j++)
+        for (int j = 0; j < 100; j++)
+        {
+            position.z = j;
+            position.x = Mathf.PerlinNoise(Time.deltaTime * j * 5, 10) * 20;
+            Instantiate(cubePrefab, position, Quaternion.identity);
+            for (int i = 0; i < 100; i++)
             {
-                position.z = j;
-                position.x = Mathf.PerlinNoise(Time.deltaTime * j * 5, 10) * 20;
+                position.x = i;
+                position.y = Mathf.PerlinNoise(Time.deltaTime * i * 5, 10) * 20;
                 Instantiate(cubePrefab, position, Quaternion.identity);
-                for (int i = 0; i < 100; i++)
-                {
-                    position.x = i;
-                    position.y = Mathf.PerlinNoise(Time.deltaTime * i * 5, 10) * 20;
-                    Instantiate(cubePrefab, position, Quaternion.identity);
 
-                }
             }
         }
-
-
     }
-    
-
-
-
-
 }
