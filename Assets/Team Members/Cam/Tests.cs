d8e0a2e2efc13640ac9605d646fadffee5616ced
CameronBondeSAE <30306370+CameronBondeSAE@users.noreply.github.com>
2022-10-05 11:58:56 +1100

    New input system

--- Assets/Team Members/Cam/Tests.cs
+++ Assets/Team Members/Cam/Tests.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Tests : MonoBehaviour
 {
@@ -13,6 +14,9 @@ public class Tests : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (InputSystem.GetDevice<Keyboard>().wKey.wasPressedThisFrame)
+        {
+            
+        }
     }
 }
