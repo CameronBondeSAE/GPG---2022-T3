8cdfdbf06e689629c857e060d7b8852adaaae7fd
trash <102205@student.sae.edu.au>
2022-10-05 15:47:35 +1100

    Lloyd folder + Perlin test

---
+++ Assets/Team Members/Lloyd/PerlinScript.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PerlinScript : MonoBehaviour
+{
+    
+    public GameObject cubePrefab;
+
+    private float x;
+    private float y;
+    
+    public Vector3 position;
+    
+    
+    
+    private void Start()
+    {
+            for (int j = 0; j < 100; j++)
+            {
+                position.z = j;
+                position.x = Mathf.PerlinNoise(Time.deltaTime * j * 5, 10) * 20;
+                Instantiate(cubePrefab, position, Quaternion.identity);
+                for (int i = 0; i < 100; i++)
+                {
+                    position.x = i;
+                    position.y = Mathf.PerlinNoise(Time.deltaTime * i * 5, 10) * 20;
+                    Instantiate(cubePrefab, position, Quaternion.identity);
+
+                }
+            }
+        }
+
+
+    }
+    
+
+
+
+
+}
