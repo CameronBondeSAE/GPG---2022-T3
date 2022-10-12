69c0ee439bc2ea882fc059fcd2d4e995b5167e3d
MarcusGeorgiou <1027503@student.sae.edu.au>
2022-10-12 16:40:40 +1100

    starting item spawning

--- Assets/Team Members/Marcus/Item Spawning/ItemSpawning.cs
+++ Assets/Team Members/Marcus/Item Spawning/ItemSpawning.cs
@@ -2,8 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class testing : MonoBehaviour
+public class ItemSpawning : MonoBehaviour
 {
+    public GameObject pickup;
+    
     // Start is called before the first frame update
     void Start()
     {
