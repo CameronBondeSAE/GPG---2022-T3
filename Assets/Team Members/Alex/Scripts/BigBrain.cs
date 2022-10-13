60c0a9c20d031844cb0b29fdacf363f05279e1c4
Alex <1024087@student.sae.edu.au>
2022-10-13 14:50:18 +1100

    Update BigBrain.cs

--- Assets/Team Members/Alex/Scripts/BigBrain.cs
+++ Assets/Team Members/Alex/Scripts/BigBrain.cs
@@ -7,7 +7,7 @@ public class BigBrain : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //hello
     }
 
     // Update is called once per frame
