a971f280cc4182d882fa9af55877d3c009d8526b
trashgamedev <thefreshkeeper@gmail.com>
2022-10-13 16:03:09 +1100

    added spawning walls (hacky)

--- Assets/Team Members/Lloyd/Scripts/PerlinScript.cs
+++ Assets/Team Members/Lloyd/Scripts/PerlinScript.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Random = UnityEngine.Random;
 
 public class PerlinScript : MonoBehaviour
 {
@@ -18,18 +20,27 @@ public class PerlinScript : MonoBehaviour
 
     [Header("Noise Settings")] [SerializeField]
     private int numCube;
-
-    [SerializeField] private int cubeSize;
+    
     [SerializeField] private float zoomX;
     [SerializeField] private float zoomY;
     [SerializeField] private float scale;
 
-    public GameObject parentObj;
+    [Header("Parent Objs (fix later)")] [SerializeField]
+    public GameObject terrainParent;
+    public GameObject itemParent;
+    public GameObject environmentParent;
+    
 
     private Vector3 cubePos;
     private Renderer cubeRend;
     private GameObject cubeSpawned;
     private bool isHighest;
+
+    private void Start()
+    {
+        GenerateTerrain();
+        
+    }
 
 
     public void GenerateTerrain()
@@ -56,39 +67,55 @@ public class PerlinScript : MonoBehaviour
 
                 if (perlinNoise > 0.4)
                 {
-                    GameObject go = Instantiate(cubePrefab, cubePos, Quaternion.identity) as GameObject;
-                    go.transform.SetParent(parentObj.transform);
+                    GameObject cube = Instantiate(cubePrefab, cubePos, Quaternion.identity) as GameObject;
+                    cube.transform.SetParent(terrainParent.transform);
 
 
                     if (perlinNoise > .6f)
                     {
-                        cubeRend = go.GetComponent<Renderer>();
+                        cubeRend = cube.GetComponent<Renderer>();
                         cubeRend.material.color = Color.red;
                     }
 
                     if (perlinNoise < .5f)
                     {
-                        cubeRend = go.GetComponent<Renderer>();
+                        cubeRend = cube.GetComponent<Renderer>();
                         cubeRend.material.color = Color.black;
+                        
+                        
+                    }
+                    
+                    //changes the size of the bottom cubes to stretch towards the ground (ostensibly prevents running underneath & fills gaps)
+                    Resize(3f, new Vector3(0f, Mathf.Abs(-1), 0f));
+                    void Resize(float amount, Vector3 direction)
+                    {
+                        cube.transform.position += direction * amount / 2;
+                        cube.transform.localScale += direction * amount;
                     }
                 }
             }
+            
         }
 
         PlaceGround();
+        PlaceWalls();
     }
+    
+    
 
     void SpawnItems()
     {
-        float a = 0f;
-        float b = 1f;
+        itemPos = new Vector3(cubePos.x, 1, cubePos.z);
+        
+        float a = -.5f;
+        float b = .5f;
         float perlinNoise = Mathf.PerlinNoise(a, b * scale);
 
         if (perlinNoise > 0.0f)
         {
             GameObject item =
-                Instantiate(itemPrefab, cubePos, Quaternion.identity) as GameObject;
-            item.transform.SetParent(parentObj.transform);
+                Instantiate(itemPrefab, itemPos, Quaternion.identity) as GameObject;
+            item.transform.SetParent(itemParent.transform);
             cubeRend = item.GetComponent<Renderer>();
             cubeRend.material.color = Color.yellow;
         }
@@ -96,11 +123,43 @@ public class PerlinScript : MonoBehaviour
 
     void PlaceWalls()
     {
+        
+        GameObject wall01 = Instantiate(cubePrefab, new Vector3(cubePos.x/2, cubePos.y, cubePos.z+.5f), Quaternion.identity);
+        wall01.name = "Wall01";
+        wall01.transform.localScale = new Vector3(cubePos.x, cubePos.y*scale*2, 1);
+       
+        cubeRend = wall01.GetComponent<Renderer>();
+        cubeRend.material.color = Color.blue;
+        
+        GameObject wall02 = Instantiate(cubePrefab, new Vector3(cubePos.x+.5f, cubePos.y, cubePos.z/2), Quaternion.identity);
+        wall02.name = "Wall02";
+        wall02.transform.localScale = new Vector3(1, cubePos.y*scale*2, cubePos.z);
+        cubeRend = wall02.GetComponent<Renderer>();
+        wall02.transform.SetParent(environmentParent.transform);
+        cubeRend.material.color = Color.blue;
+        
+        GameObject wall03 = Instantiate(cubePrefab, new Vector3(cubePos.x/2, cubePos.y, -.5f), Quaternion.identity);
+        wall03.name = "Wall03";
+        wall03.transform.localScale = new Vector3(cubePos.x, cubePos.y*scale*2, 1);
+        wall03.transform.SetParent(environmentParent.transform);
+        cubeRend = wall03.GetComponent<Renderer>();
+        cubeRend.material.color = Color.blue;
+        
+        GameObject wall04 = Instantiate(cubePrefab, new Vector3(-.5f, cubePos.y, cubePos.z/2), Quaternion.identity);
+        wall04.name = "Wall04";
+        wall04.transform.localScale = new Vector3(1, cubePos.y*scale*2, cubePos.z);
+        wall04.transform.SetParent(environmentParent.transform);
+        cubeRend = wall04.GetComponent<Renderer>();
+        cubeRend.material.color = Color.blue;
+        
+        
+       
     }
 
     void PlaceGround()
     {
         GameObject ground = Instantiate(cubePrefab, new Vector3(cubePos.x / 2, 0, cubePos.z / 2), Quaternion.identity);
+        ground.name = "Ground";
         ground.transform.localScale = new Vector3(cubePos.x, cubePos.y, cubePos.z);
         cubeRend = ground.GetComponent<Renderer>();
         cubeRend.material.color = Color.blue;
