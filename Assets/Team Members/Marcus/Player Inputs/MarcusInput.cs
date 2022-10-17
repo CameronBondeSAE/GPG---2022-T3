efeb52eb2f90c9c4f20124c8fe2adba33d7dfeaa
MarcusGeorgiou <1027503@student.sae.edu.au>
2022-10-17 11:45:54 +1100

    Random colour

--- Assets/Team Members/Marcus/Player Inputs/MarcusInput.cs
+++ Assets/Team Members/Marcus/Player Inputs/MarcusInput.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class MarcusInput : MonoBehaviour
 {
+    public Material[] playerColour;
+    private MeshRenderer mr;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        mr = GetComponent<MeshRenderer>();
+        mr.material = playerColour[Random.Range(0, playerColour.Length)];
     }
 
     // Update is called once per frame
