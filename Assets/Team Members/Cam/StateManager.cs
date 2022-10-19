f9dadd286155c749023ade6913083f7b7c1af49e
CameronBondeSAE <30306370+CameronBondeSAE@users.noreply.github.com>
2022-10-19 15:44:25 +1100

    IControllable interface Levels folder

---
+++ Assets/Team Members/Cam/StateManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateManager : MonoBehaviour
+{
+	public void ChangeState(StateBase stateBase)
+	{
+		
+	}
+
+}
