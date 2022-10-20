a075d145a69d346eb07640341b157c47294a6a35
SAE1\1027503 <1027503@student.sae.edu.au>
2022-10-20 16:17:09 +1100

    it get size

--- Assets/Team Members/Marcus/Plant/Spreading.cs
+++ Assets/Team Members/Marcus/Plant/Spreading.cs
@@ -2,8 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Spreading : MonoBehaviour
+public class Spreading : MonoBehaviour, IFlammable
 {
+    public GameObject seedling;
+
+    private float maxSize = 1;
+    private bool canSpread;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,21 @@ public class Spreading : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //if meets a certain size, grow new plants
+        if (transform.localScale.x < maxSize)
+        {
+            transform.localScale += new Vector3(0.1f * Time.deltaTime, 0, 0.1f * Time.deltaTime);
+        }
+    }
+
+    void Spread()
+    {
+        //grow new plants
+    }
+
+    public void SetOnFire()
+    {
+        //straight die
+        //maybe scream or wither
     }
 }
