aff893cfa413e32d343144548a2f24ef2db254a9
CameronBondeSAE <30306370+CameronBondeSAE@users.noreply.github.com>
2022-10-20 14:20:48 +1100

    no message

--- Assets/Team Members/Cam/Elevator/ElevatorStopState.cs
+++ Assets/Team Members/Cam/Elevator/ElevatorStopState.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 
 public class ElevatorStopState : MonoBehaviour
 {
+	// Note that even though I want sounds and fx to play when we first stop,
+	// I don't do that here. The View should care about ME, not the other way round.
+	// So the best way to do that is to spam out events and let them sort it out
+	// ps "Action" is just a shortcut built into C# for a simple delegate definition
 	public event Action ElevatorStoppedEvent;
 
 	private void OnEnable()
--- Assets/Team Members/Cam/Elevator/Elevator_ViewModel.cs
+++ Assets/Team Members/Cam/Elevator/Elevator_ViewModel.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 
 public class Elevator_ViewModel : MonoBehaviour
 {
+	// ViewModel script are simply middlemen to help communication between the Model/Logic code,
+	// and the View which is just for superficial stuff like graphics, fx, sound etc
+	// Just subscribe to events coming from model code and hack away.
 	public ElevatorStopState elevatorStopState;
 	public AudioSource audioSource;
 	public AudioClip stoppedClip;
