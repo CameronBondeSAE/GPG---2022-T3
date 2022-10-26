b372594cda3644223789bd3cbb329f32fc40f04f
SAE1\1027503 <1027503@student.sae.edu.au>
2022-10-26 12:30:34 +1100

    well i sorta get a circle of rays but um.

--- Assets/Team Members/Marcus/Raycasts/Radar.cs
+++ Assets/Team Members/Marcus/Raycasts/Radar.cs
@@ -1,23 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.HighDefinition;
 
 public class Radar : MonoBehaviour
 {
+    public int rays;
+    private float raySpacing;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        raySpacing = 360 / rays;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Ray ray = new Ray(transform.position, transform.forward);
+        for (int i = 0; i < rays; i++)
+        {
+            Vector3 facing = Quaternion.Euler(0, i * raySpacing, 0) * transform.forward;
+            Debug.DrawRay(transform.position, facing * 10f, Color.green);
+        }
+        
+        /*Ray ray = new Ray(transform.position, transform.forward);
 
         RaycastHit hitInfo;
         Physics.Raycast(ray, out hitInfo);
         
-        Debug.DrawLine(ray.origin, hitInfo.point, Color.green);
+        Debug.DrawLine(ray.origin, hitInfo.point, Color.green);*/
     }
 }
