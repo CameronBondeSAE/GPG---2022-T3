f0f9cbd2f4ec2c1e9c6cbb3adb88c189ab2f1e97
SAE1\1027503 <1027503@student.sae.edu.au>
2022-10-27 13:06:15 +1100

    it aint be spreading

--- Assets/Team Members/Marcus/Plant/Spreading.cs
+++ Assets/Team Members/Marcus/Plant/Spreading.cs
@@ -5,9 +5,15 @@ using UnityEngine;
 public class Spreading : MonoBehaviour, IFlammable
 {
     public GameObject seedling;
+    
+    public int spreadAmount;
+    private int spreadNumber;
 
     private float maxSize = 1;
-    private bool canSpread;
+
+    private float spreadTimer;
+    private float spreadDistance;
+    private Vector3 spreadDirection;
     
     // Start is called before the first frame update
     void Start()
@@ -23,11 +29,33 @@ public class Spreading : MonoBehaviour, IFlammable
         {
             transform.localScale += new Vector3(0.1f * Time.deltaTime, 0, 0.1f * Time.deltaTime);
         }
+        else
+        {
+            print("I can grow");
+            spreadTimer = Random.Range(2f, 5f);
+            spreadTimer -= Time.deltaTime;
+
+            if (spreadTimer <= 0 && spreadNumber < spreadAmount)
+            {
+                print("I'm growing now");
+                spreadDistance = Random.Range(1, 3);
+                spreadDirection = Quaternion.Euler(0, Random.Range(0f, 360f), 0) * transform.forward;
+                
+                Spread(spreadDistance, spreadDirection);
+            }
+        }
     }
 
-    void Spread()
+    void Spread(float distance, Vector3 direction)
     {
         //grow new plants
+        Ray ray = new Ray(transform.position, direction);
+        RaycastHit hitInfo;
+        Physics.Raycast(ray, out hitInfo, distance);
+        
+        Instantiate(seedling, hitInfo.point, Quaternion.identity);
+        spreadNumber++;
+        print("plant growed");
     }
 
     public void SetOnFire()
