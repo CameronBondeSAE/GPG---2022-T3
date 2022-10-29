42d3e451094a6bf4301542c9614b9c399cdaacfd
Oscar-Skinner <1027514@student.sae.edu.au>
2022-10-29 13:53:11 +1100

    Worked on sludge explosive. Visually appealing, Functionally, goo does nothing yet.

--- Assets/Team Members/Oscar/Destructible/Sludge/SludgeStates/SludgeExplodeState.cs
+++ Assets/Team Members/Oscar/Destructible/Sludge/SludgeStates/SludgeExplodeState.cs
@@ -11,7 +11,7 @@ public class SludgeExplodeState : MonoBehaviour
     {
         //spawn sludge
         GameObject goo = Instantiate(sludgeGoo, transform.position + new Vector3(0,-1,0), quaternion.identity);
-        //goo.transform.localScale = new Vector3(Random.Range(1f,4f),0,Random.Range(1f,4f));
+        goo.transform.localScale = new Vector3(Random.Range(1f,4f),0.05f,Random.Range(1f,4f));
         
         //goo has been spawned so just destroy the remains of the canister
         DestroySludgeModel();
