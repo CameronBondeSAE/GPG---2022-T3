6953090be3ce82454789a6617f847c108bbd4c9c
Oscar-Skinner <1027514@student.sae.edu.au>
2022-11-04 14:25:08 +1100

    Explosive has a public explosive radius variable It is located on the script "ExplosiveRaycast"

--- Assets/Team Members/Oscar/Destructible/Explosion/ExplosiveRaycast.cs
+++ Assets/Team Members/Oscar/Destructible/Explosion/ExplosiveRaycast.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class ExplosiveRaycast : MonoBehaviour
 {
+    public float explodeRadius = 5f;
     public void ExplosionRaycast()
     {
         //create overlap sphere to do a raycast to set other objects on fire.
-        Collider[] colliders = Physics.OverlapSphere(transform.position, 5f);
+        Collider[] colliders = Physics.OverlapSphere(transform.position, explodeRadius);
 
         foreach (Collider item in colliders) 
         {
