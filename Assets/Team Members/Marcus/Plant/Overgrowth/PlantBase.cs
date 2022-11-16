55e252ae97522b1901dec55756ced81d3047facd
SAE1\1027503 <1027503@student.sae.edu.au>
2022-11-17 10:52:45 +1100

    I think the plant burns?

--- Assets/Team Members/Marcus/Plant/Overgrowth/PlantBase.cs
+++ Assets/Team Members/Marcus/Plant/Overgrowth/PlantBase.cs
@@ -1,11 +1,20 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Oscar;
 using UnityEngine;
 
 namespace Marcus
 {
     public class PlantBase : MonoBehaviour, IFlammable
     {
+        public FlammableComponent fireness;
+
+        private void OnEnable()
+        {
+            fireness.SetOnFireEvent += SetOnFire;
+        }
+
         // Start is called before the first frame update
         void Start()
         {
@@ -20,7 +29,13 @@ namespace Marcus
         
         public void ChangeHeat(IHeatSource heatSource, float x)
         {
-            //Do dying things
+            // Maybe make a thing to smoke
+        }
+
+        void SetOnFire()
+        {
+            print("OUCHIES FIRE HURT");
+            // Combust and shrivel
         }
     }
 }
