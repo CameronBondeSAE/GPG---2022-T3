371f168644f1835feceadbbc5cea3b12bd32ecd7
CameronBondeSAE <30306370+CameronBondeSAE@users.noreply.github.com>
2022-11-23 18:03:08 +1100

    no message

--- Assets/Team Members/Cam/HacksAndTests.cs
+++ Assets/Team Members/Cam/HacksAndTests.cs
@@ -9,12 +9,12 @@ public class HacksAndTests : MonoBehaviour
     // Start is called before the first frame update
     IEnumerator Start()
     {
+	    FindObjectOfType<MarcusTerrain>().Clear();
 	    yield return new WaitForSeconds(0.5f);
 	    foreach (HQ hq in FindObjectsOfType<HQ>())
 	    {
 		    hq.DestroyLand(12f);
 	    }
-	    FindObjectOfType<MarcusTerrain>().Clear();
     }
 
     // Update is called once per frame
