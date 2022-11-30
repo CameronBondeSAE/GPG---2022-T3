e6ba6544991383bababb93eaeee62bc232ed2c1f
Oscar-Skinner <1027514@student.sae.edu.au>
2022-11-30 15:05:19 +1100

    Radar fixing

--- Assets/Team Members/Oscar/Raycasts/Radar_View.cs
+++ Assets/Team Members/Oscar/Raycasts/Radar_View.cs
@@ -13,28 +13,33 @@ namespace Oscar
         public float lineThickness = 1f;
         public float intensity = 1f;
 
+        public bool scanVisual;
         //override used because it is from the inherited script
+        
         public override void DrawShapes(Camera cam)
         {
-            base.DrawShapes(cam);
-            
-            //draw the lines in the game space.
-            using (Draw.Command(Camera.main))
+            if (scanVisual)
             {
-                //aspects for the lines
-                Draw.ResetAllDrawStates();
-                Draw.BlendMode = ShapesBlendMode.Additive;
-                Draw.Thickness = lineThickness;
-                Draw.LineGeometry = LineGeometry.Billboard;
-                Draw.ThicknessSpace = ThicknessSpace.Meters;
-                Draw.Color = colour * intensity;
-                Draw.Position = transform.position;
-                Draw.Rotation = Quaternion.identity;
-
-                //draw the lines
-                for (int i = 0; i < 360; i++)
+                base.DrawShapes(cam);
+                            
+                //draw the lines in the game space.
+                using (Draw.Command(Camera.main))
                 {
-                    Draw.Line(dir, Vector3.zero, Color.clear, Draw.Color);
+                    //aspects for the lines
+                    Draw.ResetAllDrawStates();
+                    Draw.BlendMode = ShapesBlendMode.Additive;
+                    Draw.Thickness = lineThickness;
+                    Draw.LineGeometry = LineGeometry.Billboard;
+                    Draw.ThicknessSpace = ThicknessSpace.Meters;
+                    Draw.Color = colour * intensity;
+                    Draw.Position = transform.position;
+                    Draw.Rotation = Quaternion.identity;
+    
+                    //draw the lines
+                    for (int i = 0; i < 360; i++)
+                    {
+                        Draw.Line(dir, Vector3.zero, Color.clear, Draw.Color);
+                    }
                 }
             }
         }
