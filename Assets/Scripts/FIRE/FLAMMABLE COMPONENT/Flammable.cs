9d342d2a357b32ae1895f0c8cf9747aa9cefa6d9
Luke Archbold <1024504@student.sae.edu.au>
2022-11-30 11:08:26 +1100

    Update Flammable.cs

--- Assets/Scripts/FIRE/FLAMMABLE COMPONENT/Flammable.cs
+++ Assets/Scripts/FIRE/FLAMMABLE COMPONENT/Flammable.cs
@@ -3,36 +3,52 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Lloyd;
+using Random = UnityEngine.Random;
 
 public class Flammable : MonoBehaviour, IHeatSource
 {
     //Flammable Component assumes gameObj also has a HealthComponent attached
-    private Health healthComp;
-    
-    public GameObject flameModelPrefab;
-    private FlameModel flameModel;
-    
+    private Health _healthComp;
+
+    private FlameModel _flameModel;
+    public GameObject flamePrefab;
+
     //determines how much is inflicted through ChangeHeat
     //does this go here? does flamethrower hold this info? do different objects' fire hurt more? etc
     [Header("Fire Damage")] [SerializeField]
     private float fireDamage;
 
-    [Header("Current Heat Level")]
+    [Header("Current Heat Level")] [SerializeField]
     //object's current heat level
-    public float heatLevel;
+    private float heatLevel;
+
+    public float HeatLevel
+    {
+	    get => heatLevel;
+	    set
+	    {
+		    if (value < 0) heatLevel = 0;
+		    else
+		    {
+			    heatLevel = value;
+			    if (ticking) return;
+			    ticking = true;
+			    StartCoroutine(FireTick());
+		    }
+	    }
+    }
 
     [Header("Object Set On Fire Heat Level")]
     //the threshold at which object is actually alight
     //this is where "On Fire" animations, sounds, logic, etc are activated
     //spawns Flame prefab
-    [SerializeField]
     public float heatThreshold;
 
     //some objects burn faster than others
     //tracked with this float, multiplies ChangeHeat
     [SerializeField] private float heatMultiplier;
 
-    private bool burning;
+    private bool _burning;
 
     //effectively the fire's HP / lifespan
     //adds current health to use as fuel
@@ -44,112 +60,120 @@ public class Flammable : MonoBehaviour, IHeatSource
     //determines how big the instantiated flame should be
     [SerializeField] private float radius;
 
-    List<GameObject> fireList = new List<GameObject>();
+    List<GameObject> fireList = new ();
 
+    private bool ticking;
+
+    public event Action CoolDown;
+    
     private void OnEnable()
     {
-        healthComp = GetComponent<Health>();
+        _healthComp = GetComponent<Health>();
 
-        fuel += healthComp.GetHP();
+        fuel += _healthComp.GetHP();
     }
 
-    public void FixedUpdate()
+    private void Start()
     {
-        if (burning)
-            healthComp.ChangeHP(-fireDamage * 0.2f);
-
-        Cool();
+	    if (heatLevel > 0)
+	    {
+		    ticking = true;
+		    StartCoroutine(FireTick());
+	    }
     }
 
-    public void SetOnFire()
+    private IEnumerator FireTick()
+    {
+	    // Randomly distributing ticks across frames to prevent lagging.
+	    int rand = Random.Range(1, 50);
+	    for (int i = 0; i < rand; i++)
+	    {
+		    yield return new WaitForFixedUpdate();
+	    }
+	    
+	    while (HeatLevel > 0)
+	    {
+		    yield return new WaitForSeconds(1f); //Prevents this from being called every frame, also means we don't have to account for fixedDeltaTime;
+		    if (_burning) _healthComp.ChangeHP(-fireDamage); //Burn damage
+		    ChangeHeat(this, -coolRate); //Cooling
+		    CoolDown?.Invoke();
+	    }
+
+	    ticking = false;
+    }
+
+    private void SetOnFire()
     {
         if (fireList.Count > 0)
         {
             return;
         }
 
-        OnSetOnFire();
-        heatLevel += fireDamage;
+        SetOnFireFunction();
+        HeatLevel += fireDamage;
 
-        if (flameModelPrefab != null)
-        {
-            GameObject fire = Instantiate(flameModelPrefab, transform.position, Quaternion.identity) as GameObject;
-            fire.transform.SetParent(transform);
-            flameModel = fire.GetComponent<FlameModel>();
-            if (flameModel != null)
-            {
-                flameModel.SetFlameStats(fireDamage, fuel, radius);
+        if (flamePrefab == null) return;
+        GameObject fire = Instantiate(flamePrefab, transform.position, Quaternion.identity) as GameObject;
+        fire.transform.SetParent(transform);
+        _flameModel = fire.GetComponent<FlameModel>();
+        
+        if (_flameModel == null) return;
+        _flameModel.SetFlameStats(fireDamage, fuel, radius);
                 
 
-                fireList.Add(fire);
-            }
-        }
+        fireList.Add(fire);
     }
 
     public void Extinguish()
     {
-        burning = false;
-        heatLevel = 0;
+        _burning = false;
+        HeatLevel = 0;
 
         foreach (GameObject fire in fireList)
         {
-            flameModel = fire.GetComponentInChildren<FlameModel>();
-            flameModel.SetFlameStats(0, 0, 0);
+            _flameModel = fire.GetComponentInChildren<FlameModel>();
+            _flameModel.SetFlameStats(0, 0, 0);
             Destroy(fire);
         }
-        
+
         fireList.Clear();
     }
 
-
-    public void ChangeHeat(IHeatSource x, float y)
+    public void ChangeHeat(IHeatSource source, float amount)
     {
         //heatMultiplier is only added for positive change heat input
         //not for water / extinguishers
-        if (y > 0)
+        if (amount > 0)
         {
-            heatLevel += y * heatMultiplier;
+	        HeatLevel += amount * heatMultiplier;
+            if (HeatLevel > heatThreshold && !_burning)
+            {
+	            _burning = true;
+	            SetOnFire();
+            }
         }
-
         else
         {
-            heatLevel += y;
+	        HeatLevel += amount;
         }
-
-        if (heatLevel >= heatThreshold && !burning)
-        {
-            burning = true;
-            SetOnFire();
-        }
-
-        if (heatLevel <= heatThreshold || fuel <= 0)
-        {
-            burning = false;
-        }
-
-        if (heatLevel <= 0)
-        {
-            heatLevel = 0;
-           // Extinguish();
-        }
-    }
-
-    public event Action CoolDown;
-    public void OnCoolDown()
-    {
-        CoolDown?.Invoke();
-    }
-    
-    private void Cool()
-    {
-        ChangeHeat(this, -coolRate * Time.deltaTime);
-        CoolDown?.Invoke();
+        
+        if (HeatLevel > heatThreshold) return;
+        if (!(HeatLevel <= 0 || fuel <= 0)) return;
+        
+        _burning = false;
+        HeatLevel = 0;
+        // Extinguish();
     }
 
     public event Action SetOnFireEvent;
 
-    public void OnSetOnFire()
+    private void SetOnFireFunction()
     {
         SetOnFireEvent?.Invoke();
+    }
+    
+    public void OnCoolDown()
+    {
+	    CoolDown?.Invoke();
     }
 }
