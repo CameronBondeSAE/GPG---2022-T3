fa1ba7c2ee0545a5b8fd0ca9762d79a8901ca092
Luke Archbold <1024504@student.sae.edu.au>
2022-12-08 17:17:32 +1100

    Update LobbyUIManager.cs

--- Assets/Scripts/Managers/LobbyUIManager.cs
+++ Assets/Scripts/Managers/LobbyUIManager.cs
@@ -348,8 +348,8 @@ namespace Ollie
                 if (scene == SceneManager.GetActiveScene())
                 {
                     //SceneManager.UnloadSceneAsync(scene);
-                    UnloadSceneClientRpc(scene.name);
-                    //NetworkManager.Singleton.SceneManager.UnloadScene(scene);
+                    // UnloadSceneClientRpc(scene.name);
+                    NetworkManager.Singleton.SceneManager.UnloadScene(scene);
                 }
             }
             
