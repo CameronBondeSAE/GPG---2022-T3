9adecffcca6b65fdc27b0ce2c9c57000dbf41fac
Oscar-Skinner <1027514@student.sae.edu.au>
2022-12-08 11:06:57 +1100

    border fix

--- Assets/Team Members/Oscar/Scripts/SpawnEnvironment.cs
+++ Assets/Team Members/Oscar/Scripts/SpawnEnvironment.cs
@@ -28,8 +28,10 @@ namespace Oscar
         public void SpawnTheEnvironment(Vector3 prefabPosition, int amount, float scale, GameObject borderParent)
         {
             //spawn all the walls
+            
+            //problem:
             GameObject firstWall =Instantiate(borderPrefab,
-                new Vector3(-1, prefabPosition.y, (amount / 2)), Quaternion.identity);
+                new Vector3(-0.5f, prefabPosition.y, (amount / 2)), Quaternion.identity);
             firstWall.name = "firstWall";
             firstWall.transform.localScale = new Vector3(1, prefabPosition.y * scale * 2, prefabPosition.z + 2);
             firstWall.transform.SetParent(borderParent.transform);
@@ -46,8 +48,9 @@ namespace Oscar
             thirdWall.transform.localScale = new Vector3(1, prefabPosition.y * scale * 2, prefabPosition.z + 2);
             thirdWall.transform.SetParent(borderParent.transform);
             
+            //problem:
             GameObject fourthWall = Instantiate(borderPrefab,
-                new Vector3((amount / 2), prefabPosition.y, -1), Quaternion.identity);
+                new Vector3((amount / 2), prefabPosition.y, -0.5f), Quaternion.identity);
             fourthWall.name = "fourthWall";
             fourthWall.transform.localScale = new Vector3(prefabPosition.x + 2, prefabPosition.y * scale * 2, 1);
             fourthWall.transform.SetParent(borderParent.transform);
